9d01d28c1f5e050dd56cd18337f01543673cff5d
mohitfulkar <mohitfulkar@gmail.com>
2024-10-09 18:21:16 +0530

    rotate poster

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./style.css";
+import Poster from "../images/poster.png";
 
 const AboutUs = () => {
   return (
     <div className="about-us  bg p-64 pt-16 space-x-12 flex justify-between ">
-      <div className="poster hidden md:block lg:block"></div>
+      <div className="poster hidden md:block lg:block">
+        <img src={Poster} alt="" />
+      </div>
       <div className="poster-heading space-y-7 pt-32 text-center text-2xl md:justify-center ">
         <p>Seemlessly go through the Profile</p>
         <p>Best Map services</p>
